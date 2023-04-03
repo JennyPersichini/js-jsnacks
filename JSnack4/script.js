4c0b668f8ed0ca13b433d0bd8c9ba5d5d4078a13
JennyPersichini <nenny1096@gmail.com>
2023-04-03 15:51:36 +0200

    Complete JSnack4

--- JSnack4/script.js
+++ JSnack4/script.js
@@ -1,2 +1,16 @@
 //In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 //chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+let listaInvitati = ["Emiliano", "Jenny", "Jimmy", "Valerio", "Marco", "Paola", "Lucia"];
+
+let nomeInvitato = prompt("Come ti chiami??");
+
+for (let i = 0 ; i < listaInvitati.length; i++) {
+
+    if (listaInvitati.includes(nomeInvitato)) {
+        console.log("Sei presente nella lista, entra pure e buona serata!!");
+    } else {
+        console.log("Non sei stato invitato, sparisci!")
+    }
+    
+} 
