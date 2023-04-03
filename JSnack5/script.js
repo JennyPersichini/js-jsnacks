2aa00f9eacb2230e0d83c6145e6af66d963c22cc
JennyPersichini <nenny1096@gmail.com>
2023-04-03 16:25:11 +0200

    Complete JSnack5

--- JSnack5/script.js
+++ JSnack5/script.js
@@ -1,2 +1,18 @@
 //Crea un array vuoto.
-//Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. Stampa l'array alla fine.
+//Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. 
+//Stampa l'array alla fine.
+
+let array = [];
+
+for (i = 0; i < 6; i++) {
+
+    let number = prompt('inserisci numero');
+    let num = parseInt(number);
+
+    if (num % 2 == 1) {
+        array.push(num);
+    }
+
+}
+
+console.log(array);
